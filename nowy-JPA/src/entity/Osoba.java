90c8e0c9f9db5612ea696b1009efb3fdddc4a779
Okońska Karolina <kokonska@win.coig.com>
2017-10-29 20:13:50 +0100

    usuniecie pol

--- nowy-JPA/src/entity/Osoba.java
+++ nowy-JPA/src/entity/Osoba.java
@@ -33,9 +33,8 @@ public class Osoba implements Serializable {
 	
 	@OneToOne(fetch=FetchType.LAZY, mappedBy="osoba", cascade=CascadeType.ALL)
 	private User user;
-	private String email;
 
-	private String nazwisko;
+
 	public long getId() {
 		return id;
 	}
